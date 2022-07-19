aaa83cebc65c83afacefb3b660bea6ca704401c1
loopbr4ker <73437603+loopbr4ker@users.noreply.github.com>
2022-07-19 16:39:23 +0200

    feat: init
    - vite
    - solidjs
    - tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import type { Component } from 'solid-js';
+import { Editor } from './editor';
+
+const App: Component = () => {
+  return <Editor />;
+};
+
+export default App;
---
+++ src/editor/editor.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'solid-js';
+
+const Editor: Component = () => {
+  console.log();
+
+  return <p class="text-4xl text-green-700 text-center py-20">Editor!</p>;
+};
+
+export default Editor;
